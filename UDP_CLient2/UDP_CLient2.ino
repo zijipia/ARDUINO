c940a5f3d0446b1b7c44e195772d26ce7a491496
Ziji <Zijipia@gmail.com>
2024-08-29 02:06:31 +0700

    2 client

--- UDP_CLient2/UDP_CLient2.ino
+++ UDP_CLient2/UDP_CLient2.ino
@@ -4,25 +4,18 @@
 
 byte mac[] = {0x00, 0xBA, 0xDA, 0xDF, 0xAC, 0x02};
 IPAddress ip(192, 168, 1, 55);
-// IPAddress myDns(192, 168, 1, 1);
-// IPAddress gateway(192, 168, 1, 1);
-// IPAddress subnet(255, 255, 0, 0);
 IPAddress serverIp(192, 168, 1, 177); 
 unsigned int serverPort = 8888;
 
 EthernetUDP Udp;
 
-unsigned long previousMillis = 0;
-const long interval = 5000;
-
 void setup() {
   Ethernet.init(5);
   Ethernet.begin(mac, ip);
-  // Ethernet.begin(mac, ip, myDns, gateway, subnet);
   Udp.begin(serverPort);  
   Serial.begin(115200);
   Serial.println("UDP Client started");
-  sendPacket("Hello from client");
+  sendPacket("Hello from COM8");
 }
 
 void loop() {
@@ -30,14 +23,17 @@ void loop() {
   if (packetSize) {
     char replyBuffer[255];
     Udp.read(replyBuffer, 255);
-    Serial.println("Reply from");
+    Serial.print("Reply from");
     Serial.println(Udp.remoteIP());
 
     Serial.println(replyBuffer);
 
-    // Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
-    // Udp.write("ReplyBuffer");
-    // Udp.endPacket();
+  }
+
+  // Kiểm tra nếu có dữ liệu từ Serial
+  if (Serial.available() > 0) {
+    String message = Serial.readStringUntil('\n'); // Đọc toàn bộ dòng từ Serial
+    sendPacket(message.c_str()); // Gửi dữ liệu đến server
   }
 }
 
--- udp_client/udp_client.ino
+++ udp_client/udp_client.ino
@@ -4,25 +4,18 @@
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 IPAddress ip(192, 168, 1, 177);
-// IPAddress myDns(192, 168, 1, 1);
-// IPAddress gateway(192, 168, 1, 1);
-// IPAddress subnet(255, 255, 0, 0);
 IPAddress serverIp(192, 168, 1, 55); 
 unsigned int serverPort = 8888;
 
 EthernetUDP Udp;
 
-unsigned long previousMillis = 0;
-const long interval = 5000;
-
 void setup() {
   Ethernet.init(5);
   Ethernet.begin(mac, ip);
-  // Ethernet.begin(mac, ip, myDns, gateway, subnet);
-  Udp.begin(serverPort);
+  Udp.begin(serverPort);  
   Serial.begin(115200);
   Serial.println("UDP Client started");
-  sendPacket("Hello from client");
+  sendPacket("Hello from COM5");
 }
 
 void loop() {
@@ -30,19 +23,17 @@ void loop() {
   if (packetSize) {
     char replyBuffer[255];
     Udp.read(replyBuffer, 255);
-    Serial.println("Reply from server: ");
+    Serial.print("Reply from");
+    Serial.println(Udp.remoteIP());
+
     Serial.println(replyBuffer);
 
-    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
-    Udp.write("ReplyBuffer");
-    Udp.endPacket();
   }
 
-  unsigned long currentMillis = millis();
-
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
-    sendPacket("Another message from client");
+  // Kiểm tra nếu có dữ liệu từ Serial
+  if (Serial.available() > 0) {
+    String message = Serial.readStringUntil('\n'); // Đọc toàn bộ dòng từ Serial
+    sendPacket(message.c_str()); // Gửi dữ liệu đến server
   }
 }
 
