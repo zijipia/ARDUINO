d2627f5c5c0ce04c2f0e64ffece0f7044a1a8f29
Ziji <Zijipia@gmail.com>
2024-11-20 22:06:11 +0700

    Update UDP_CLient2.ino

--- UDP_CLient2/UDP_CLient2.ino
+++ UDP_CLient2/UDP_CLient2.ino
@@ -1,44 +1,15 @@
-#include <SPI.h>
-#include <Ethernet2.h>
-#include <EthernetUdp2.h>
-
-byte mac[] = {0x00, 0xBA, 0xDA, 0xDF, 0xAC, 0x02};
-IPAddress ip(192, 168, 1, 55);
-IPAddress serverIp(192, 168, 1, 177); 
-unsigned int serverPort = 8888;
-
-EthernetUDP Udp;
-
 void setup() {
-  Ethernet.init(5);
-  Ethernet.begin(mac, ip);
-  Udp.begin(serverPort);  
-  Serial.begin(115200);
-  Serial.println("UDP Client started");
-  sendPacket("Hello from COM8");
+  // Khởi tạo Serial
+  Serial.begin(9600);
+  // Khởi tạo bộ sinh số ngẫu nhiên với seed từ giá trị ngẫu nhiên của analog (nếu cần)
+  randomSeed(analogRead(0));
 }
 
 void loop() {
-  int packetSize = Udp.parsePacket();
-  if (packetSize) {
-    char replyBuffer[255];
-    Udp.read(replyBuffer, 255);
-    Serial.print("Reply from ");
-    Serial.println(Udp.remoteIP());
-
-    Serial.println(replyBuffer);
-
-  }
-
-  if (Serial.available() > 0) {
-    String message = Serial.readStringUntil('\n');
-    sendPacket(message.c_str());
-  }
-}
-
-void sendPacket(const char *message) {
-    Udp.beginPacket(serverIp, serverPort);
-    Udp.write(message);
-    Udp.endPacket();
-    Serial.print(".");
+  // Sinh số ngẫu nhiên từ 0 đến 100
+  int randomNumber = random(0, 101);
+  // Xuất số ngẫu nhiên ra Serial
+  Serial.println(randomNumber);
+  // Chờ 5 giây
+  delay(5000);
 }
